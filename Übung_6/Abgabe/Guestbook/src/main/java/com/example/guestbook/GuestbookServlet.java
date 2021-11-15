729d920e8b5e817100e6f2a175cf69530598bbe2
prescherm <m.prescher@gantner-instruments.com>
2021-11-15 12:16:49 +0100

    added exercise 6 beginnings

---
+++ Übung_6/Abgabe/Guestbook/src/main/java/com/example/guestbook/GuestbookServlet.java
@@ -0,0 +1,50 @@
+package com.example.guestbook;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "guestbook_overview", value = "/guestbook_overview")
+public class GuestbookServlet extends HttpServlet {
+    private List<GuestBookEntry> guestBookEntries = new ArrayList<GuestBookEntry>();
+
+    public void init() {
+        guestBookEntries.add(new GuestBookEntry("Jeremias", "jere.huan@hotmail.com", "Geil"));
+        guestBookEntries.add(new GuestBookEntry("Heel", "heel.huan@hotmail.com", "Geil"));
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<title>MySportClub</title>");
+        out.println("</head>");
+
+        out.println("<body>");
+        out.println("<div class='container'>");
+        for (GuestBookEntry entries : guestBookEntries) {
+            out.println("<div class='row'>");
+            out.println("<p>" + entries.getName() + "</p>");
+            out.println("<p>" + entries.getEmail() + "</p>");
+            out.println("<p>" + entries.getText() + "</p>");
+            out.println("<div/>");
+        }
+        out.print("</div>");
+        out.println("</body>");
+
+        out.println("</html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ Übung_6/Abgabe/Guestbook/target/Guestbook-1.0-SNAPSHOT/js/main.js
@@ -0,0 +1,207 @@
+/**
+ * Created by Marco 05/11/2021
+ */
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    if (document.images != null) {
+        for (let i = 0; i < document.images.length; i++) {
+            document.images[i].addEventListener("click", function () {
+                openPopup(this);
+            });
+        }
+    }
+});
+
+function openPopup(imgEl) {
+    let winWidth = imgEl.width + 15;
+    let winHeight = imgEl.height + 40;
+
+    const y = window.top.outerHeight / 2 + window.top.screenY - (winHeight / 2);
+    const x = window.top.outerWidth / 2 + window.top.screenX - (winWidth / 2);
+    let newWindow = window.open("", "", "width=" + winWidth + ",height=" + winHeight + ",top=" + y + ", left=" + x);
+
+    newWindow.document.write(
+        "<head>" +
+        "<title>Image</title>" +
+        "</head>" +
+
+        "<body>" +
+        "<div>" +
+        "<img src='" + imgEl.src + "' alt='" + imgEl.alt + "' style='width: " + imgEl.width + "; height: " + imgEl.height + "'/>" +
+        "<input type='button' value='Close' onclick='self.close()'/>" +
+        "</div>" +
+        "</body>"
+    );
+
+    newWindow.onload = function () {
+        newWindow.focus();
+    };
+}
+
+function validateInputs() {
+    let elObj = {
+        firstNameEl: document.getElementById("firstName"),
+        lastNameEl: document.getElementById("lastName"),
+        addressEl: document.getElementById("address"),
+        countryEl: document.getElementById("country"),
+        userIDEl: document.getElementById("userID"),
+        emailEl: document.getElementById("email"),
+        passEl: document.getElementById("password"),
+        confirmPassEl: document.getElementById("confirmPassword"),
+        sportCatEl: document.getElementById("sportCategories"),
+    };
+
+    // default checks --------------------------------------------------------------------------------------------------
+    if (!validateStandardElems([elObj.firstNameEl, elObj.lastNameEl, elObj.addressEl, elObj.countryEl])) {
+        return false;
+    }
+
+    if (!validateRadios()) {
+        return false;
+    }
+
+    if (!validateStandardElems([elObj.userIDEl, elObj.emailEl, elObj.passEl, elObj.confirmPassEl, elObj.sportCatEl])) {
+        return false;
+    }
+
+    if (!validateCheckBoxes()) {
+        return false;
+    }
+
+
+    // extended checks -------------------------------------------------------------------------------------------------
+    if (!validateUserID(elObj.userIDEl)) {
+        return false;
+    }
+
+    if (!validatePassword(elObj.passEl, elObj.confirmPassEl)) {
+        return false;
+    }
+
+    if (!validateEmail(elObj.emailEl)) {
+        return false;
+    }
+
+    return true;
+}
+
+//#region default check functions --------------------------------------------------------------------------------------
+function validateStandardElems(elemArr) {
+    for (let i = 0; i < elemArr.length; i++) {
+        let el = elemArr[i];
+
+        if (el.value == null || el.value === "") {
+            setVisualValidation(el, false);
+            el.focus();
+
+            return false;
+        }
+
+        if (el.id !== "userID" && el.id !== "email" && el.id !== "password" && el.id !== "confirmPassword") {
+            setVisualValidation(el, true);
+        }
+    }
+
+    return true;
+}
+
+function validateRadios() {
+    let radioElArr = document.querySelectorAll('input[name="gender"]');
+    let isValid = document.querySelectorAll('input[name="gender"]:checked').length > 0;
+
+    for (let i = 0; i < radioElArr.length; i++) {
+        setVisualValidation(radioElArr[i], isValid);
+    }
+
+    setCustomValidation(document.getElementById("gender-feedback"), isValid);
+
+    return isValid;
+}
+
+function validateCheckBoxes() {
+    let checkElArr = document.querySelectorAll('input[name="userCategory"]');
+    let isValid = document.querySelectorAll('input[name="userCategory"]:checked').length > 0;
+
+    for (let i = 0; i < checkElArr.length; i++) {
+        let el = checkElArr[i];
+
+        if (isValid) {
+            el.removeAttribute("required");
+        } else {
+            el.setAttribute("required", "");
+        }
+
+        setVisualValidation(el, isValid);
+    }
+
+    setCustomValidation(document.getElementById("sportCategories-feedback"), isValid);
+
+    return isValid;
+}
+
+//#endregion
+
+//#region extended check functions -------------------------------------------------------------------------------------
+function validateString(str, regex) {
+    return regex.exec(str) !== null;
+}
+
+function validateUserID(el) {
+    let regex = /^[a-zA-Z_]{5,9}$/;
+    let isValid = validateString(el.value, regex);
+
+    setVisualValidation(el, isValid);
+
+    return isValid;
+}
+
+function validatePassword(elPass, elPassConf) {
+    let regex = /^[a-zA-Z][a-zA-Z0-9_]{7,8}$/;
+    let isValid = validateString(elPass.value, regex) && elPass.value === elPassConf.value;
+
+    setVisualValidation(elPass, isValid);
+    setVisualValidation(elPassConf, isValid);
+
+    return isValid;
+}
+
+function validateEmail(el) {
+    // [a-zA-Z](\.?[\w-]+)*                 - name
+    // @
+    // (([a-zA-Z](-*[\w]+)*)\.)*            - sub domains
+    // [a-zA-Z](-*[a-zA-Z0-9]){3,63}\.      - domain name
+    // [a-zA-Z]{2,4}                        - TLD
+    let regex = /^[a-zA-Z](\.?[\w-]+)*@(([a-zA-Z](-*[\w]+)*)\.)*[a-zA-Z](-*[a-zA-Z0-9]){3,63}\.[a-zA-Z]{2,4}$/;
+    let isValid = validateString(el.value, regex);
+
+    setVisualValidation(el, isValid);
+
+    return isValid;
+}
+
+//#endregion
+
+//#region visual feedback functions ------------------------------------------------------------------------------------
+function setVisualValidation(el, isValid) {
+    if (el == null) {
+        return;
+    }
+
+    if (isValid) {
+        el.classList.remove("is-invalid");
+        el.classList.add("is-valid");
+    } else {
+        el.classList.remove("is-valid");
+        el.classList.add("is-invalid");
+    }
+}
+
+function setCustomValidation(el, isValid) {
+    if (isValid) {
+        el.classList.remove("displayBlock");
+    } else {
+        el.classList.add("displayBlock");
+    }
+}
+
+//#endregion
