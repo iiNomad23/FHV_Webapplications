b525d01f68b365872f8ddbd6b21a86e288446b1d
prescherm <m.prescher@gantner-instruments.com>
2021-11-15 13:51:48 +0100

    minor change!

--- Übung_6/Abgabe/Guestbook/src/main/java/com/example/guestbook/GuestbookServlet.java
+++ Übung_6/Abgabe/Guestbook/src/main/java/com/example/guestbook/GuestbookServlet.java
@@ -20,8 +20,7 @@ public class GuestbookServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        // Hello
+        
         PrintWriter out = response.getWriter();
         out.println("<html>");
 
