7539cf108a6ae82e2b0e6a965357da2ecc9d1bfb
prescherm <m.prescher@gantner-instruments.com>
2021-11-19 11:00:30 +0100

    added exercise 7 beginnings

---
+++ Übung_7/Abgabe/MySportClub/src/main/java/com/example/mySportClub/GuestBookEntry.java
@@ -0,0 +1,41 @@
+package com.example.guestbook;
+
+public class GuestBookEntry {
+    private String name;
+    private String email;
+    private String message;
+
+    public GuestBookEntry() {
+        this("", "", "");
+    }
+
+    public GuestBookEntry(String name, String email, String text) {
+        this.name = name;
+        this.email = email;
+        this.message = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
